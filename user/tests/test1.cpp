21c4a8b41d5f3d2a55653d63bea10a2a71bd83b3
aishprr <apremrenu@gmail.com>
2017-04-24 15:10:03 -0400

    test starter

--- user/tests/test1.cpp
+++ user/tests/test1.cpp
@@ -1,7 +1,75 @@
 #include "../../lib/mutex/mutex_type.h"
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NRA 62                 /* number of rows in matrix A */
+#define NCA 15                 /* number of columns in matrix A */
+#define NCB 7                  /* number of columns in matrix B */
+
+int matrix_multiply()
+{
+  int tid, nthreads, i, j, k, chunk;
+  double a[NRA][NCA];   /* matrix A to be multiplied */
+  double b[NCA][NCB];           /* matrix B to be multiplied */
+  double c[NRA][NCB];           /* result matrix C */
+
+  chunk = 10;             /* set loop iteration chunk size */
+
+  /*** Spawn a parallel region explicitly scoping all variables ***/
+  #pragma omp parallel shared(a,b,c,nthreads,chunk) private(tid,i,j,k)
+  {
+    tid = omp_get_thread_num();
+    if (tid == 0)
+      {
+      nthreads = omp_get_num_threads();
+      printf("Starting matrix multiple example with %d threads\n",nthreads);
+      printf("Initializing matrices...\n");
+      }
+    /*** Initialize matrices ***/
+    #pragma omp for schedule (static, chunk) 
+    for (i=0; i<NRA; i++)
+      for (j=0; j<NCA; j++)
+        a[i][j]= i+j;
+    #pragma omp for schedule (static, chunk)
+    for (i=0; i<NCA; i++)
+      for (j=0; j<NCB; j++)
+        b[i][j]= i*j;
+    #pragma omp for schedule (static, chunk)
+    for (i=0; i<NRA; i++)
+      for (j=0; j<NCB; j++)
+        c[i][j]= 0;
+
+    /*** Do matrix multiply sharing iterations on outer loop ***/
+    /*** Display who does which iterations for demonstration purposes ***/
+    printf("Thread %d starting matrix multiply...\n",tid);
+    #pragma omp for schedule (static, chunk)
+    for (i=0; i<NRA; i++)    
+      {
+      printf("Thread=%d did row=%d\n",tid,i);
+      for(j=0; j<NCB; j++)       
+        for (k=0; k<NCA; k++)
+          c[i][j] += a[i][k] * b[k][j];
+      }
+    }   /*** End of parallel region ***/
+
+  /*** Print results ***/
+  printf("******************************************************\n");
+  printf("Result Matrix:\n");
+  for (i=0; i<NRA; i++)
+    {
+    for (j=0; j<NCB; j++) 
+      printf("%6.2f   ", c[i][j]);
+    printf("\n"); 
+    }
+  printf("******************************************************\n");
+  printf ("Done.\n");
+}
+
 
 int main() {
   mutex_t mu;
   mutex_create(&mu);
+
   return 1;
 }
