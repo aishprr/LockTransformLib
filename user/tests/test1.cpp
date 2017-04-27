1999ee9ec952cf3c95e27ecd638b76ee00e397f2
aishprr <apremrenu@gmail.com>
2017-04-27 00:21:35 -0400

    apparently I have to flush -- but that slows it down a lot

--- user/tests/test1.cpp
+++ user/tests/test1.cpp
@@ -4,14 +4,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <cstddef>
+#include <sched.h>
 
-#define PARALLEL_ADDS (10)
+#define PARALLEL_ADDS (100)
 
 int matrix_multiply()
 {
   return 1;
 }
-
 
 int main() {
 
@@ -19,16 +19,17 @@ int main() {
   mutex_create(&mu);
 
   int i = 0;
-  #pragma omp parallel shared(mu)
+  #pragma omp parallel for num_threads(PARALLEL_ADDS)
+  for(int u = 0; u < PARALLEL_ADDS; u++)
   {
     int tid = omp_get_thread_num();
-
-    #pragma omp for schedule(static)
-    for(int u = 0; u < PARALLEL_ADDS; u++) {
-      mutex_lock(&mu);
-      i = i + 1;
-      mutex_unlock(&mu);
-    }
+    int cpu_num = sched_getcpu();
+    printf("thread %d on CPU %d\n", tid, cpu_num);
+  
+    printf("thread number inside %d\n", tid);
+    mutex_lock(&mu);
+    i = i + 1;
+    mutex_unlock(&mu);
   }
 
   printf("i = %d\n", i);
