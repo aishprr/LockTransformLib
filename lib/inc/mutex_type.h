487997ef2485a63fcb2eaec02aa5282f53c6158f
aishprr <apremrenu@gmail.com>
2017-05-08 22:13:16 -0400

    all of the things need to be volatile

--- lib/inc/mutex_type.h
+++ lib/inc/mutex_type.h
@@ -2,17 +2,10 @@
 #define _MUTEX_TYPE_H
 
 
-// #ifdef __cplusplus
-// extern "C" {
-// #endif
-
-//#define MUTEX_TRANSACTION_RTM (1)
-
 typedef struct mutex {
 
-// TODO: to add to CFLAGS -D in Makefile
 #ifdef MUTEX_SPIN
-  int lock;
+  volatile int lock;
 #endif
 #ifdef MUTEX_QUEUE_VOL
   volatile int lock_count;
@@ -28,7 +21,7 @@ typedef struct mutex {
   volatile int lock;
 #endif
 #ifdef MUTEX_TRANSACTION_HLE
-  unsigned int lock;
+  volatile unsigned int lock;
 #endif
 
 } mutex_t;
